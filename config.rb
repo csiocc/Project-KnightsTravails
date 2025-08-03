099797a26a0b5419a61a8089a0a3776f76357f65
Christian <christian.siegrist@open-circle.ch>
2025-08-03 16:30:36 +0200

    updated config comment

--- config.rb
+++ config.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 module Config
-  WINDOW_SIZE = 1024 # do not change this (atm it only runs on 512px)
+  WINDOW_SIZE = 1024 # only tested 512 and 1024 but feel free to try other resolutions
   TILE_COUNT = 2116 # try 4, 16, 64, 256, 529, 1024, 2116, 4225 (4225 is still not 100% working)
 end
