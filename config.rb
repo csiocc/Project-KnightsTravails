5a7548e4a004f1e075de5d72a66af3e155148456
Christian <christian.siegrist@open-circle.ch>
2025-08-03 16:14:01 +0200

    added a draw line function to show the path

--- config.rb
+++ config.rb
@@ -1,4 +1,4 @@
 module Config
   WINDOW_SIZE = 512 # do not change this (it only runs on 512px)
-  TILE_COUNT = 4225 # try 4, 16, 64, 256, 529, 1024, 2116, 4225 (4225 is still not 100% working)
+  TILE_COUNT = 1024 # try 4, 16, 64, 256, 529, 1024, 2116, 4225 (4225 is still not 100% working)
 end
--- main.rb
+++ main.rb
@@ -20,6 +20,8 @@ set height: Config::WINDOW_SIZE
 knight = Knight.new(test.tile_size)
 knight.current_tile = test.topleft
 
+path_lines = []
+
 on :key_down do |event|
   # Read the key event
   case event.key
@@ -30,18 +32,61 @@ on :key_down do |event|
 end
 
 on :mouse_down do |event|
+  # clear old lines
+  path_lines.each(&:remove)
+  path_lines.clear
+
   # x and y coordinates of the mouse button event
   cords = { x: event.x, y: event.y }
 
   target_tile = test.find_tile(cords)
+
+  # next if no tile clicked or the same as start
+  next unless target_tile && target_tile != knight.current_tile
+
+  start_tile = knight.current_tile
+  tile_size = test.tile_size
+  center_offset = tile_size / 2
+
+  # helper function to draw a line
+  draw_line = lambda do |from_tile, to_tile, color|
+    start_px = from_tile.draw_cords
+    end_px = to_tile.draw_cords
+
+    start = { x: start_px[:x] + center_offset, y: start_px[:y] + center_offset }
+    end_center = { x: end_px[:x] + center_offset, y: end_px[:y] + center_offset }
+
+    dr = to_tile.cords[0] - from_tile.cords[0]
+    dc = to_tile.cords[1] - from_tile.cords[1]
+
+    corner = if dr.abs > dc.abs # vertikal
+               { x: start[:x], y: end_center[:y] }
+             else #  horizontal
+               { x: end_center[:x], y: start[:y] }
+             end
+
+    path_lines << Line.new(
+      x1: start[:x], y1: start[:y], x2: corner[:x], y2: corner[:y],
+      width: 3, color: color, z: 10
+    )
+    path_lines << Line.new(
+      x1: corner[:x], y1: corner[:y], x2: end_center[:x], y2: end_center[:y],
+      width: 3, color: color, z: 10
+    )
+  end
+
   if knight.move_legal?(target_tile.cords)
+    draw_line.call(start_tile, target_tile, 'red')
     knight.move(target_tile)
     p "Moved to #{target_tile.cords} in 1 move"
   else
     path = knight.find_path(target_tile, test)
     if path
-      path.each { |tile| p tile.cords }
-      p "Path found in #{path.length} moves:"
+      p "Path found in #{path.length - 1} moves:"
+      path.each { |tile| p " -> #{tile.cords}" }
+      path.each_cons(2) do |from_tile, to_tile|
+        draw_line.call(from_tile, to_tile, 'lime')
+      end
       knight.move(target_tile) # Move the knight to the target
     else
       p 'No path found'
