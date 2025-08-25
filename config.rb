52287653f3b2f892641f9961ebb2ddc2414c3f25
Christian <christian.siegrist@open-circle.ch>
2025-08-25 10:09:50 +0200

    updated readme

--- config.rb
+++ config.rb
@@ -2,5 +2,5 @@
 
 module Config
   WINDOW_SIZE = 1024 # only tested 512 and 1024 but feel free to try other resolutions
-  TILE_COUNT = 2116 # try 4, 16, 64, 256, 529, 1024, 2116, 4225 (4225 is still not 100% working)
+  TILE_COUNT = 64 # try 4, 16, 64, 256, 529, 1024, 2116, 4225 (4225 is still not 100% working)
 end
